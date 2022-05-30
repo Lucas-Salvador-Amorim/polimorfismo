744dfb05986ac9a6cff72b4682e4612c6abb494c
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-05-30 17:59:58 +0000

    polimorfismo

---
+++ Quadrilatero.java
@@ -0,0 +1,11 @@
+public class Quadrilatero{
+
+    public void calcularArea(){
+        System.out.println("Calculos das areas: ");
+    }
+
+    public Quadrilatero(){
+
+    }
+
+}
---
+++ Retangulo.java
@@ -0,0 +1,8 @@
+public class Retangulo extends Quadrilatero {
+
+    @Override
+    public void calcularArea() {
+        System.out.println("Base * Altura ");
+    }
+    
+}
